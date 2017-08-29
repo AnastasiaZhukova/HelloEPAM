65e254648a9d96678f0e2d46a6139064455081da
Anastasia Zhukova <azhuk.mail@gmail.com>
2017-08-29 13:08:10 +0300

    Hello EPAM

---
+++ app/src/main/java/anastasiazhukova/helloepam/MainActivity.java
@@ -0,0 +1,20 @@
+package anastasiazhukova.helloepam;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void onClick(View view) {
+        Toast toastHelloEpam= Toast.makeText(getApplicationContext(),"Hello EPAM!",Toast.LENGTH_SHORT);
+        toastHelloEpam.show();
+    }
+}
